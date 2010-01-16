6813fab2245713263ef514ecf1f4369db364f191
Alex Sharp <ajsharp@gmail.com>
2010-01-15 20:17:53 -0800

    added a test database and automigrate to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,16 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
 require 'request_lumberjack'
 require 'spec'
 require 'spec/autorun'
 
+
 Spec::Runner.configure do |config|
+
+  config.before(:all) do
+    RequestLumberjack::Config.database "sqlite3://#{Dir.pwd}/request_lumberjack_test.sqlite3"
+    DataMapper.auto_migrate!
+  end
   
 end
