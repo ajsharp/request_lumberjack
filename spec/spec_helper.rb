5c55471f6292afee3211561519435eb486580590
Alex Sharp <ajsharp@gmail.com>
2010-01-16 20:15:30 -0800

    lots of changes to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,45 @@
+Bundler.require_env(:test)
+
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'rubygems'
+require 'rack/test'
 require 'request_lumberjack'
-require 'spec'
-require 'spec/autorun'
+require 'action_controller'
+
+Webrat.configure do |config|
+  config.mode = :rack
+  config.application_port = 4567
+end
 
 Spec::Runner.configure do |config|
-  def get_rails_response(opts = {})
-    env = { 'REQUEST_METHOD' => 'GET', 'REQUEST_URI' => 'http://example.com/controller/action' }.merge(opts)
-    request  = ActionController::Request.new env
+  config.include(Rack::Test::Methods)
+  config.before(:all) do
+    RequestLumberjack::Config.database "sqlite3://#{Dir.pwd}/request_lumberjack_test.sqlite3"
+    DataMapper.auto_migrate!
+  end
+
+  def mock_env(opts = {})
+    { 'REQUEST_METHOD' => 'GET', 'REQUEST_URI' => 'http://example.com/controller/action' }.merge(opts)
+  end
+
+  def mock_rails_response(opts = {})
+    request  = ActionController::Request.new mock_env(opts)
     response = ActionController::Response.new
     response.request = request
     response
   end
 
-  config.before(:all) do
-    RequestLumberjack::Config.database "sqlite3://#{Dir.pwd}/request_lumberjack_test.sqlite3"
-    DataMapper.auto_migrate!
+  def mock_rack_response
+    Rack::Response.new
+  end
+
+  def app
+    RequestLumberjack.app
+  end
+
+  def response
+    last_response
   end
   
 end
