fec5a72f009440bc50aa59f21773975ca987948f
Alex Sharp <ajsharp@gmail.com>
2010-01-18 13:08:18 -0800

    some tweaking to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rack/test'
+require 'init'
 require 'request_lumberjack'
 
 Webrat.configure do |config|
@@ -13,8 +14,11 @@ end
 
 Spec::Runner.configure do |config|
   config.include(Rack::Test::Methods)
-  config.before(:all) do
+  config.before :all do
     RequestLumberjack::Config.database "sqlite3://#{Dir.pwd}/request_lumberjack_test.sqlite3"
+  end
+
+  config.before(:each) do
     DataMapper.auto_migrate!
   end
 
@@ -41,8 +45,8 @@ Spec::Runner.configure do |config|
     last_response
   end
 
-  def valid_params
-    { 'request_method' => 'get', 'request_uri' => '/', 'status' => 200 }
+  def valid_params(opts = {})
+    { 'request_method' => 'get', 'request_uri' => '/', 'status' => 200 }.merge(opts)
   end
   
 end
